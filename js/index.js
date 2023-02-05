1161295242a9ac7a33640fa63774b039c2afeeac
Agajan Sahatov <agajan.st@gmail.com>
2023-02-05 09:31:20 +0800

    build the navbar component

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
+// const collapsibles = document.querySelectorAll(".collapsible");
+// collapsibles.forEach((item) =>
+// 	item.addEventListener("click", function () {
+// 		this.classList.toggle("collapsible--expanded");
+// 	})
+// );
+
+// const collapsibles = document.querySelectorAll(".collapsible__toggler");
+// collapsibles.forEach((item) =>
+// 	item.addEventListener("click", function () {
+// 		this.parentNode.classList.toggle("collapsible--expanded");
+// 	})
+// );
+
 const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-	item.addEventListener("click", function () {
-		this.classList.toggle("collapsible--expanded");
-	})
-);
+
+collapsibles.forEach((item) => {
+	const toggler = item.querySelector(".collapsible__toggler");
+	toggler.addEventListener("click", function () {
+		item.classList.toggle("collapsible--expanded");
+	});
+});
