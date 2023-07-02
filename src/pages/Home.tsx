159d38f3668bb662dfd85c134ef9492c4cad20ba
Agajan Sahatov <agajan.st@gmail.com>
2023-07-02 10:39:21 +0800

    Created the Home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Banner from "../sections/Banner";
+
+const Home = () => {
+	return (
+		<>
+			<Navbar />
+			<Banner />
+		</>
+	);
+};
+
+export default Home;
