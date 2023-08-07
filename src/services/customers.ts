fc16b875c28e6204d875e000e9355b2cb77f94a5
Agajan Sahatov <agajan.st@gmail.com>
2023-08-08 00:16:29 +0800

    Converted to React Finally

--- src/services/customers.ts
+++ src/services/customers.ts
@@ -6,7 +6,7 @@ let customers: ICustomer[] = [
 		photo: "/images/meylis.png",
 		company: "Entrepreneur",
 		comment:
-			"I am so happy to recommend Agajan Sahatov and his company. They delivered an excellent website (a landing page), which is very beautiful, responsive and blazingly fast. The team is very professional, easy to work with and has good results.",
+			"I am so happy to recommend Agajan Sahatov and his company. They delivered an excellent website (a landing page), which is very beautiful, responsive and blazingly fast. The team is very professional, easy to work with and has good results...",
 	},
 ];
 
